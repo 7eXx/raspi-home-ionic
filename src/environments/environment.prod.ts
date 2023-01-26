b1656f30901b03bdf7e3442f5f890c26c6cbf664
7eXx <marco.tex@live.com>
2023-01-26 22:16:12 +0100

    added capacitor dependency

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  mqtt: {
+    protocol: 'ws',
+    server: '192.168.0.95',
+    port: 9001,
+    clientId: 'ionic-client',
+    timeout: 3000,
+    path: '/mqtt',
+    statusTopic: 'mqtt/status',
+    commandTopic: 'mqtt/command',
+  }
 };
