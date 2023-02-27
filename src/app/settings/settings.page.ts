e7baada19f1babac5984572e0b9b4192486a37e5
Texx <marco.tex@live.com>
2023-02-27 22:29:37 +0100

    feat: add settings page

---
+++ src/app/settings/settings.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+})
+export class SettingsPage implements OnInit {
+
+  title = 'Settings';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
