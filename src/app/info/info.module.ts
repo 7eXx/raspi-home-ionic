94de1c70306d7bf43773f4e6d4544eb28204c912
7eXx <marco.tex@live.com>
2023-08-23 23:18:22 +0200

    improve position for gauge in info page

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -9,15 +9,17 @@ import { InfoPageRoutingModule } from './info-routing.module';
 import { CpuComponent } from '../components/cpu/cpu.component';
 import { MemoryComponent } from '../components/memory/memory.component';
 import { DiskComponent } from '../components/disk/disk.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    NgxGaugeModule,
-    InfoPageRoutingModule,
-  ],
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        NgxGaugeModule,
+        InfoPageRoutingModule,
+        SharedModule,
+    ],
   declarations: [
     InfoPage,
     CpuComponent,
