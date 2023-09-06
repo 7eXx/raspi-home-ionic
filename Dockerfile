b8470f2fc0b3f73e05f6bd094b40cdafe482ec9d
7eXx <marco.tex@live.com>
2023-09-06 22:30:26 +0200

    feat: docker build image script

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# ----------------------
+# build from source
+# ----------------------
+FROM node:18 as build
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm ci
+
+COPY . .
+RUN npm run build
+
+# ---------------------
+# run with nginx
+# ---------------------
+FROM nginx:stable
+
+COPY --from=build /app/www /usr/share/nginx/html
+
+EXPOSE 80
+
+
---
+++ build-push.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+[ -f .env ] && . ./.env
+[ -f .env.local ] && . ./.env.local
+
+set -e
+
+showHelp()
+{
+  echo "Build docker image for backend raspi home and push it to custom registry"
+  echo "Syntax: ./build-push.sh [--no-push]"
+  echo
+  echo "options:"
+  echo
+  echo "--no-push     Avoid pushing build image"
+  echo "-h            Print this help guide"
+}
+
+setArgs()
+{
+  while [ "${1:-}" != "" ]; do
+    case "$1" in
+      "-h" | "--help")
+        showHelp
+        exit 0
+        ;;
+      "--arm")
+        platform="linux/arm/v6"
+        ;;
+      "--no-push")
+        no_push="no-push"
+        ;;
+      *)
+        showHelp
+        exit 0
+        ;;
+    esac
+    shift
+  done
+}
+
+setArgs()
+{
+  while [ "${1:-}" != "" ]; do
+    case "$1" in
+      "-h" | "--help")
+        showHelp
+        exit 0
+        ;;
+      "--no-push")
+        no_push="no-push"
+        ;;
+      *)
+        showHelp
+        exit 0
+        ;;
+    esac
+    shift
+  done
+}
+
+# If private docker registry is not defined, login to Docker.io
+if [ -z ${PRIVATE_DOCKER_REGISTRY} ]; then
+    cat ./.docker_pass | docker login -u $USERNAME --password-stdin
+    image_name=${USERNAME}/${DOCKER_IMAGE}
+else
+    image_name=${PRIVATE_DOCKER_REGISTRY}/${DOCKER_IMAGE}
+fi
+
+full_image_name=${image_name}:${DOCKER_IMAGE_TAG}
+echo "docker image - ${full_image_name}"
+
+current_branch=$(git branch --show-current)
+branch_image_name=${image_name}:${current_branch}
+echo "docker image branch - ${branch_image_name}"
+
+commit_hash=$(git log -1 --format=%h)
+commit_image_name=${image_name}:${commit_hash}
+echo "docker image commit hash - ${commit_image_name}"
+
+docker build . \
+    --platform ${platform} \
+    -t ${full_image_name} \
+    -t ${branch_image_name} \
+    -t ${commit_image_name}
+
+if [ -z "${no_push}" ]; then
+  docker push ${full_image_name}
+  docker push ${branch_image_name}
+  docker push ${commit_image_name}
+fi
+
+exit 0
